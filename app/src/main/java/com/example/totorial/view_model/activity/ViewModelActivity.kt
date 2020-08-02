d59a07357287056f8b756e2cf21a8fd6c9a82df1
Tanachart Arayachutinunt <tanachart.ar@gmail.com>
2020-08-03 02:46:53 +0700

    LiveData with dataBlinding

--- app/src/main/java/com/example/totorial/view_model/activity/ViewModelActivity.kt
+++ app/src/main/java/com/example/totorial/view_model/activity/ViewModelActivity.kt
@@ -3,7 +3,6 @@ package com.example.totorial.view_model.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.totorial.R
 import com.example.totorial.databinding.ActivityViewModelBinding
@@ -16,15 +15,9 @@ class ViewModelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_model)
+        binding.lifecycleOwner = this
         viewModel = ViewModelProvider(this).get(ViewModelActivityViewModel::class.java)
-        viewModel.getCount().observe(this, Observer {
-            binding.tvCounter.text = it.toString()
-        })
-        binding.apply {
-            btnCounter.setOnClickListener{
-                viewModel.increaseCount()
-            }
-        }
+        binding.mainViewModel = viewModel
     }
 
 }
